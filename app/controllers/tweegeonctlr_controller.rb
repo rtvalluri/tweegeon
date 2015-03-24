f2b88df74e1a6b544efc954a68d965dbacbeadf1
Raviteja <valluriraviteja@gmail.com>
2015-03-24 23:21:17 +0530

    Boosttrap classes added

--- app/controllers/tweegeonctlr_controller.rb
+++ app/controllers/tweegeonctlr_controller.rb
@@ -4,10 +4,10 @@ class TweegeonctlrController < ApplicationController
 		puts username
 		if username.to_s != ''
 			client = Twitter::REST::Client.new do |config|
-				config.consumer_key        = "consumer key comes here"
-				config.consumer_secret     = "consumer secret comes here"
-				config.access_token        = "access token goes here"
-				config.access_token_secret = "access token secret comes here"
+				config.consumer_key        = "aGzYCh7xPSXOYdOOPDAOSdEgn"
+				config.consumer_secret     = "Lhn7bn3CCOA09lOdmiyOGeLwc0fjJlPpSSkZ4O0fCzj3Q8WW59"
+				config.access_token        = "2693666210-tRwFYClnoD2fcgPVUtf3HBQJY3Kkj9cnwywRLjG"
+				config.access_token_secret = "KNnwLzm47YIzpIuQ1hiK2jsUMtpqgcGvbYTa1DSP44eeO"
 			end
 			@tweets = client.user_timeline(username)
 		end
