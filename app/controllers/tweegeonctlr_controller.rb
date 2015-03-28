a7ad4e19a6c8550a760e788705c62a7e2efb7d5d
Raviteja <valluriraviteja@gmail.com>
2015-03-29 00:24:38 +0530

    Exception handling added

--- app/controllers/tweegeonctlr_controller.rb
+++ app/controllers/tweegeonctlr_controller.rb
@@ -1,5 +1,6 @@
 class TweegeonctlrController < ApplicationController
 	def index
+		flag = 1
 		username = params[:user][:name] rescue nil
 		puts username
 		if username.to_s != ''
@@ -9,7 +10,26 @@ class TweegeonctlrController < ApplicationController
 				config.access_token        = "2693666210-tRwFYClnoD2fcgPVUtf3HBQJY3Kkj9cnwywRLjG"
 				config.access_token_secret = "KNnwLzm47YIzpIuQ1hiK2jsUMtpqgcGvbYTa1DSP44eeO"
 			end
-			@tweets = client.user_timeline(username)
+			begin
+				user_obj = client.user(username)
+			rescue => e
+				puts "****************Error occured***************"
+				puts "#{e}"
+				puts "**************************************************"
+				@response = e
+				flag = 0
+			end
+			if flag == 1
+				if username.eql?(user_obj.screen_name)
+					@tweets = client.user_timeline(username)
+				else
+					if username.eql?(user_obj.name)
+						@tweets = client.user_timeline(username)
+					end
+				end
+			else
+				@tweets = ''
+			end
 		end
 	end	
 end
